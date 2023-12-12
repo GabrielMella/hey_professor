3f319c6965e73c8204b88e3a2808ca76a98afb5b
GabrielMella <gabrielmella09@gmail.com>
2023-12-12 00:41:34 +0000

    hey-5: Not be able to like more than one time

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -24,3 +24,21 @@ it('should be able to like a question', function () {
     ]);
 
 });
+
+it('should not be able to like more than one time', function () {
+    // arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    // act
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+    post(route('question.like', $question));
+
+    // assert
+    expect($user->votes()->where('question_id', $question->id)->get())
+        ->toHaveCount(1);
+});
