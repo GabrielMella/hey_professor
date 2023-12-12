9694eec1ad3bf3953a203a0e04b4a74cbc338465
GabrielMella <gabrielmella09@gmail.com>
2023-12-12 22:04:39 +0000

    hey-5: Adding like button to our interface

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,12 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
     use HasFactory;
     //protected $guarded = [];
     //protected $fillable = ['question'];
+
+    /**
+     * @return HasMany<Vote>
+     */
+    public function votes(): HasMany
+    {
+        return $this->hasMany(Vote::class);
+    }
+
+    public function likes(): Attribute
+    {
+        // Irá no banco de dados, somar todos os likes e retornar
+        return new Attribute(get: fn () => $this->votes()->sum('like'));
+    }
+
+    public function unlikes(): Attribute
+    {
+        return new Attribute(get: fn () => $this->votes()->sum('unlike'));
+    }
 }
