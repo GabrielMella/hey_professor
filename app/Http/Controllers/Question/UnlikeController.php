49de8a34cdd2a11e024645ff3f0c1d8ae0c519f5
GabrielMella <gabrielmella09@gmail.com>
2023-12-13 02:58:17 +0000

    hey-5: Unlike button

--- app/Http/Controllers/Question/UnlikeController.php
+++ app/Http/Controllers/Question/UnlikeController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question, Vote};
-use Illuminate\Http\{RedirectResponse};
+use App\Models\Question;
+use Illuminate\Http\{RedirectResponse, Request};
 
-class LikeController extends Controller
+class UnlikeController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
         // Dessa forma: Question $question , implementamos o findOrFail() diretamente.
-        user()->like($question);
+        user()->unlike($question);
 
         return back();
     }
--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, assertDatabaseHas, post};
+use function Pest\Laravel\{actingAs, assertDatabaseHas, assertDatabaseMissing, post};
 
 it('should be able to like a question', function () {
     // arrange
@@ -37,6 +37,46 @@ it('should not be able to like more than one time', function () {
     post(route('question.like', $question));
     post(route('question.like', $question));
     post(route('question.like', $question));
+
+    // assert
+    expect($user->votes()->where('question_id', $question->id)->get())
+        ->toHaveCount(1);
+});
+
+/* Unlike */
+it('should be able to unlike a question', function () {
+    // arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    // act
+    post(route('question.unlike', $question))
+        ->assertRedirect(); // Valida o status Code Http
+
+    // assert
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'like'        => 0,
+        'unlike'      => 1,
+        'user_id'     => $user->id,
+    ]);
+
+});
+
+it('should not be able to unlike more than one time', function () {
+    // arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+
+    // act
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
+    post(route('question.unlike', $question));
 
     // assert
     expect($user->votes()->where('question_id', $question->id)->get())
