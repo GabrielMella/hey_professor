e27710effac1e11bac703b4b153b33bfa5a87af9
GabrielMella <gabrielmella09@gmail.com>
2023-12-13 03:22:26 +0000

    hey-5: Handling N+1 issue

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -21,14 +20,4 @@ class Question extends Model
         return $this->hasMany(Vote::class);
     }
 
-    public function likes(): Attribute
-    {
-        // Irá no banco de dados, somar todos os likes e retornar
-        return new Attribute(get: fn () => $this->votes()->sum('like'));
-    }
-
-    public function unlikes(): Attribute
-    {
-        return new Attribute(get: fn () => $this->votes()->sum('unlike'));
-    }
 }
